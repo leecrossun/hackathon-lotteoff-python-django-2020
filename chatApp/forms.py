cf33426644449fe32e7d7bd98635a2682618cb1b
aaa <janu01ary@gmail.com>
2020-10-09 02:05:20 +0900

    채팅 models, forms, css 추가

--- chatApp/forms.py
+++ chatApp/forms.py
@@ -4,9 +4,16 @@ from .models import Room, Message
 class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
-        fields = {'user', 'user_read', 'admin_read', }
+        fields = {'user', 'user_read', 'admin_read', 'last_message',}
 
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = {'body',}
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['body'].label = ""
+        self.fields['body'].widget.attrs.update({
+            'class': 'enter-message',
+            'placeholder': '1000자 미만으로 입력해주세요.'
+        })
