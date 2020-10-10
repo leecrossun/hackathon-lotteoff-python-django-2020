5cfca1b15f4f8cc8a61bec03785dc804147331c5
hyosunLee <leecrossun@gmail.com>
2020-10-10 16:08:30 +0900

    first

--- driveApp/models.py
+++ driveApp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class DriveThru(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     shop = models.CharField(max_length=20) # 지점명 (지점선택에서 받아오기,  직접 여기서 입력 x)
-    items = models.TextField() # 상품목록 (장바구니에서 받아오기, 직접 여기서 입력 x)
     state = models.IntegerField(default=0) # 픽업 준비 상황 (신청완료 = 0 (디폴트) / 상품준비중 = 1 / 준비완료, 픽업대기 = 2 / 픽업완료 = 3)
-    pick_date = models.DateTimeField() # 픽업날짜 (사용자가 선택)
+    pick_date =models.CharField(max_length=20)
+    pick_day = models.CharField(max_length=20, default=1)
     pub_date = models.DateTimeField(auto_now=True) # 작성시간 (자동기입)
--- driveApp/views.py
+++ driveApp/views.py
@@ -3,7 +3,9 @@ from django.core.paginator import Paginator
 from .models import DriveThru
 from django.http import Http404, HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
-
+from shop.models import Product
+from cart.models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 def home(request):
     return render(request, 'home.html')
@@ -23,8 +25,21 @@ def driveThru(request):
     return render(request, 'driveThru.html', {'form':form, 'posts':posts})
 
 def new(request):
-    
-    return render(request, 'new.html')
+    cart = Cart.objects.latest('date_added')
+    cart_items = CartItem.objects.filter(cart=cart, active=True)
+    return render(request, 'new.html', dict(cart_items=cart_items))
+
+def create(request):
+    cart = Cart.objects.latest('date_added')
+    cart_items = CartItem.objects.filter(cart=cart, active=True)
+    item = CartItem.objects.filter(cart=cart, pk=1)
+    drive = DriveThru()
+    drive.author = request.user
+    drive.shop = request.POST['shop']
+    drive.pick_date = request.POST['time']
+    drive.pick_day = request.POST['date']
+    drive.save()
+    return redirect('driveThru')
 
 def delete (request, pk):
     blog = get_object_or_404(DriveThru, pk = pk)
